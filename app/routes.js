a79fde1892a44dd49e9f9f43e2ba05c76bf324a5
Millie Guest <melissag@kainos.com>
2025-11-12 11:52:37 +0000

    overdue registration start

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,8 @@ router.get('/', (req, res) => {
 
 
 // Import and attach other routes
+require('./views/later/_routes')(router);
+
 require('./views/latest/_routes')(router);
 require('./views/latest/informant/_routes')(router);
 require('./views/latest/no-informant/_routes')(router);
