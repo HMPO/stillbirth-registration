ce0fa5bc2f1a81b8cfb2d129812237166b2c3c53
Millie Guest <melissag@kainos.com>
2025-11-21 09:05:17 +0000

    round 3 - v1s

--- app/routes.js
+++ app/routes.js
@@ -16,6 +16,8 @@ require('./views/later/_routes')(router);
 
 require('./views/req-ur-r1/_routes')(router);
 
+require('./views/req-ur-r2/_routes')(router);
+
 require('./views/latest/_routes')(router);
 require('./views/latest/informant/_routes')(router);
 require('./views/latest/no-informant/_routes')(router);
