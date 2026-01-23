88eeaac7a34c4feebc68eba42440f39ddc899599
Millie Guest <melissag@kainos.com>
2026-01-23 15:50:27 +0000

    registrar view flow -v basic

--- app/routes.js
+++ app/routes.js
@@ -12,37 +12,39 @@ router.get('/', (req, res) => {
 
 
 // Import and attach other routes
-//require('./views/later/_routes')(router);
+require('./views/requisitions/later/_routes')(router);
 
-require('./views/req-ur-r1/_routes')(router);
+require('./views/requisitions/req-ur-r1/_routes')(router);
 
-require('./views/req-ur-r2/_routes')(router);
+require('./views/requisitions/req-ur-r2/_routes')(router);
 
-require('./views/req-ur-r3/_routes')(router);
+require('./views/requisitions/req-ur-r3/_routes')(router);
 
-require('./views/req-mvp-plus-minus/_routes')(router);
+require('./views/requisitions/req-mvp-plus-minus/_routes')(router);
 
-require('./views/latest/_routes')(router);
-require('./views/latest/informant/_routes')(router);
-require('./views/latest/no-informant/_routes')(router);
+require('./views/stillbirths/latest/_routes')(router);
+require('./views/stillbirths/latest/informant/_routes')(router);
+require('./views/stillbirths/latest/no-informant/_routes')(router);
 
-require('./views/v1/_routes')(router);
-require('./views/v1/informant/_routes')(router);
-require('./views/v1/no-informant/_routes')(router);
+require('./views/stillbirths/v1/_routes')(router);
+require('./views/stillbirths/v1/informant/_routes')(router);
+require('./views/stillbirths/v1/no-informant/_routes')(router);
 
 require('./views/births/v1/_routes')(router);
 
-require('./views/v2/_routes')(router);
-require('./views/v2/informant/_routes')(router);
-require('./views/v2/no-informant/_routes')(router);
+require('./views/stillbirths/v2/_routes')(router);
+require('./views/stillbirths/v2/informant/_routes')(router);
+require('./views/stillbirths/v2/no-informant/_routes')(router);
 
-require('./views/v3/_routes')(router);
-require('./views/v3/informant/_routes')(router);
-require('./views/v3/no-informant/_routes')(router);
+require('./views/stillbirths/v3/_routes')(router);
+require('./views/stillbirths/v3/informant/_routes')(router);
+require('./views/stillbirths/v3/no-informant/_routes')(router);
 
-require('./views/v4/_routes')(router);
-require('./views/v4/informant/_routes')(router);
-require('./views/v4/no-informant/_routes')(router);
+require('./views/stillbirths/v4/_routes')(router);
+require('./views/stillbirths/v4/informant/_routes')(router);
+require('./views/stillbirths/v4/no-informant/_routes')(router);
+
+require('./views/registrar-view-of-online/_routes')(router);
 
 module.exports = router;
 
---
+++ app/views/registrar-view-of-online/_routes.js
@@ -0,0 +1,26 @@
+module.exports = function(router) {
+  
+
+// Name and date
+
+
+// router.get('/latest/child-details/name-date', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'child-details', 'name-date.html'));
+// });
+
+  // TASK LIST
+  // router.get('/latest/informant/task-list-form', (req, res) => {
+  //   req.session.data = req.session.data || {};
+  //   res.render('task-list', { data: req.session.data });
+  // });
+
+
+// v2
+//route for the Last contact status
+
+
+
+
+
+//need this at the end:
+};
