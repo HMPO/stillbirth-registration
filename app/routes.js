283100095464dc59cccdf27f4f606670357ef493
Millie Guest <melissag@kainos.com>
2026-03-10 11:23:24 +0000

    added version 3

--- app/routes.js
+++ app/routes.js
@@ -46,6 +46,7 @@ require('./views/stillbirths/v4/no-informant/_routes')(router);
 
 require('./views/registrar-view-of-online/v1/_routes')(router);
 require('./views/registrar-view-of-online/v2/_routes')(router);
+require('./views/registrar-view-of-online/v3/_routes')(router);
 
 module.exports = router;
 
