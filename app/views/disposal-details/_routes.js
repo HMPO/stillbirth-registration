ecf7a09fc95b4fe5894686392e09e288f8f7cc2c
Millie Guest <melissag@kainos.com>
2026-09-17 22:02:38 +0100

    task 2 form working

--- app/views/disposal-details/_routes.js
+++ app/views/disposal-details/_routes.js
@@ -21,6 +21,25 @@ module.exports = function (router) {
     res.render('disposal-details/task1/disposal-page');
   });
 
+
+  // Task 2- saving disposal details to session data
+  // GET: Show the disposal-form page
+  router.get('/disposal-details/task2/disposal-form', function (req, res) {
+    res.render('disposal-details/task2/disposal-form');
+  });
+
+  // POST: Handle form submission and redirect to disposal-page
+  router.post('/disposal-details/task2/disposal-form', function (req, res) {
+    // The Prototype Kit automatically stores all fields in req.session.data
+    res.redirect('/disposal-details/task2/disposal-page');
+  });
+
+  // GET: Show the disposal-page
+  router.get('/disposal-details/task2/disposal-page', function (req, res) {
+    res.render('disposal-details/task2/disposal-page');
+  });
+
+
   // No POST needed for 06 if your "next" button is a link to 04 with nextId
 
 
